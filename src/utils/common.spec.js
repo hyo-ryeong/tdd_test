f4bc81973965e5584fbbdc18602133c548108d4a
hyo-ryeong <jhl9071@naver.com>
2024-08-12 12:51:43 +0900

    [3.4] 타이머 테스트

--- src/utils/common.spec.js
+++ src/utils/common.spec.js
@@ -35,3 +35,59 @@ describe('pick util 단위테스트', () => {
     expect(pick(obj)).toEqual({});
   });
 });
+
+/** 타이머 테스트  */
+// 테스트 코드는 비동기 타이머와 무관하게 동기적으로 실행
+// -> 비동기 함수가 실행되기 전에 단언이 실행됨
+// 타이머 모킹 !
+
+describe('debounce', () => {
+  //타이머 모킹 -> 0.3초 흐른 것으로 타이머 조작 -> spy 함수 호출 확인
+  beforeEach(() => {
+    /** 무조건 teardown에서 모킹을 초기화 해야함 -> 다른 테스트에 영향이 없어야함 */
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it('특정 시간이 지난 후 함수가 호출된다.', () => {
+    //함수의 호출여부를 사용할 때 spy 함수 사용
+    vi.useFakeTimers();
+
+    const spy = vi.fn();
+
+    const debounceFn = debounce(spy, 300);
+
+    vi.advanceTimersByTime(300);
+
+    expect(spy).toHaveBeenCalled();
+
+    debounceFn();
+  });
+
+  it('연이어 호출해도 마지막 호출 기준으로 지정된 타이머 시간이 지난 경우에만 함수가 호출된다.', () => {
+    //함수의 호출여부를 사용할 때 spy 함수 사용
+    vi.useFakeTimers();
+
+    const spy = vi.fn();
+
+    const debounceFn = debounce(spy, 300);
+
+    //첫번째 호출
+    vi.advanceTimersByTime(100);
+    debounceFn();
+
+    //두번째 호출
+    vi.advanceTimersByTime(200);
+    debounceFn();
+
+    //세번째 호출
+    vi.advanceTimersByTime(300);
+    debounceFn();
+
+    //네번째 호출을 했지만 실제 spy 함수는 단 한번만 호출
+    expect(spy).toHaveBeenCalled(1);
+  });
+});
