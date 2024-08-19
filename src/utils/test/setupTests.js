267c40fd990674cb851aa29b54bc26cd57a4c7c6
hyo-ryeong <jhl9071@naver.com>
2024-08-19 12:14:37 +0900

    [4.5]msw로 api 모킹하기

--- src/utils/test/setupTests.js
+++ src/utils/test/setupTests.js
@@ -5,8 +5,10 @@ import { handlers } from '@/__mocks__/handlers';
 
 /* msw */
 export const server = setupServer(...handlers);
-
+//msw 설정 적용
+// -> 테스트 환경에서 api 호출은 msw의 핸들러에 설정한 응답으로 모킹
 beforeAll(() => {
+  //서버 구동
   server.listen();
 });
 
@@ -16,6 +18,7 @@ afterEach(() => {
 });
 
 afterAll(() => {
+  //서버 종료
   vi.resetAllMocks();
   server.close();
 });
