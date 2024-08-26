f947457242da91f5356236ad95607bba8ef0e3cf
hyo-ryeong <jhl9071@naver.com>
2024-08-26 18:49:49 +0900

    [2부 5섹션] 스냅샷 테스트

--- src/pages/cart/components/tests/PageTitle.spec.jsx
+++ src/pages/cart/components/tests/PageTitle.spec.jsx
@@ -3,6 +3,38 @@ import React from 'react';
 import PageTitle from '@/pages/cart/components/PageTitle';
 import render from '@/utils/test/render';
 
-it('pageTitle 스냅샷 테스트(toMatchInlineSnapshot)', async () => {});
+/** dom 구조를 직렬화한 스냅샷 테스트 */
+it('pageTitle 스냅샷 테스트(toMatchInlineSnapshot)', async () => {
+  /* conatiner로 감싸면 div 인것처럼 활용할 수 있다. */
+  const { container } = await render(<PageTitle />);
 
-it('pageTitle 스냅샷 테스트(toMatchSnapshot)', async () => {});
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <h1
+        class="MuiTypography-root MuiTypography-h4 css-1lnl64-MuiTypography-root"
+      >
+        상품 리스트
+      </h1>
+      <div
+        style="position: fixed; z-index: 9999; top: 16px; left: 16px; right: 16px; bottom: 16px; pointer-events: none;"
+      />
+    </div>
+  `);
+});
+
+it('pageTitle 스냅샷 테스트(toMatchSnapshot)', async () => {
+  const { container } = await render(<PageTitle />);
+
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <h1
+        class="MuiTypography-root MuiTypography-h4 css-1lnl64-MuiTypography-root"
+      >
+        상품 리스트
+      </h1>
+      <div
+        style="position: fixed; z-index: 9999; top: 16px; left: 16px; right: 16px; bottom: 16px; pointer-events: none;"
+      />
+    </div>
+  `);
+});
